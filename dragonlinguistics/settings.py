44e1de5519ac41e4afbb076a89475bab31a5c4a5
KathTheDragon <kaththedragon@gmail.com>
2021-11-06 07:40:47 +0000

    Don't use os.path, we have pathlib

--- dragonlinguistics/settings.py
+++ dragonlinguistics/settings.py
@@ -12,14 +12,13 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import environ
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Read environment
 env = environ.Env()
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+environ.Env.read_env(str(BASE_DIR / '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -135,7 +134,7 @@ USE_TZ = True
 
 STATIC_URL = env('STATIC_URL')
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'dragonlinguistics', 'static')
+STATIC_ROOT = str(BASE_DIR / 'dragonlinguistics' / 'static')
 
 
 # Media root for uploaded files
