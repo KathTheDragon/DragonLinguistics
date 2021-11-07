9edb1152bce4a74984a700579bb8eb3a10656ad5
KathTheDragon <kaththedragon@gmail.com>
2021-11-07 10:25:12 +0000

    Add models to admin site

--- dragonlinguistics/admin.py
+++ dragonlinguistics/admin.py
@@ -1,7 +1,48 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.forms import ModelForm, Textarea
 
+from .models.langs import Language
 from .models.user import User
+from .models.words import Word, Sense
 
 
+admin.site.register(Language)
 admin.site.register(User, UserAdmin)
+
+textarea_attrs = {'rows':5, 'cols':40}
+
+class SenseForm(ModelForm):
+    class Meta:
+        model = Sense
+        fields = '__all__'
+        widgets = {
+            'defin': Textarea(attrs=textarea_attrs),
+            'notes': Textarea(attrs=textarea_attrs),
+        }
+
+
+class SenseInline(admin.StackedInline):
+    model = Sense
+    extra = 1
+    form = SenseForm
+
+
+class WordForm(ModelForm):
+    class Meta:
+        model = Word
+        fields = '__all__'
+        widgets = {
+            'notes': Textarea(attrs=textarea_attrs),
+        }
+
+
+@admin.register(Word)
+class WordAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'firstgloss', 'lang')
+    list_filter = ('lang__name',)
+    search_fields = ('lemma',)
+    fields = ('lang', 'lemma', 'homonym', 'type', 'notes')
+    inlines = (SenseInline,)
+    form = WordForm
+    save_on_top = True
