e00f2f2c479ec3361b99299de6813eebd5e7f315
KathTheDragon <kaththedragon@gmail.com>
2021-11-07 10:02:46 +0000

    Fix view usage

--- dragonlinguistics/urls/words.py
+++ dragonlinguistics/urls/words.py
@@ -3,17 +3,17 @@ from ..views import words
 
 app_name = 'words'
 urlpatterns = [
-    path('', words.ListWords.as_view(), name='list'),
-    path('search', words.SearchWords.as_view(), name='search'),
-    path('new/', words.NewWord.as_view(), name='new'),
+    path('', words.List.as_view(), name='list'),
+    path('search', words.Search.as_view(), name='search'),
+    path('new/', words.New.as_view(), name='new'),
     path('word/<id:lemma>/', include([
-        path('', words.ViewWord.as_view(), name='view'),
-        path('edit/', words.EditWord.as_view(), name='edit'),
-        path('delete/', words.DeleteWord.as_view(), name='delete'),
+        path('', words.View.as_view(), name='view'),
+        path('edit/', words.Edit.as_view(), name='edit'),
+        path('delete/', words.Delete.as_view(), name='delete'),
         path('<int:homonym>/', include([
-            path('', words.ViewWord.as_view(), name='view-homonym'),
-            path('edit/', words.EditWord.as_view(), name='edit-homonym'),
-            path('delete/', words.DeleteWord.as_view(), name='delete-homonym'),
+            path('', words.View.as_view(), name='view-homonym'),
+            path('edit/', words.Edit.as_view(), name='edit-homonym'),
+            path('delete/', words.Delete.as_view(), name='delete-homonym'),
         ]))
     ]))
 ]
