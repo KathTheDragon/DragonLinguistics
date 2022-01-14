e1e63b99378113cb8fb11fcecfd8a99583df0c63
KathTheDragon <kaththedragon@gmail.com>
2022-01-14 09:58:54 +0000

    Remove obsolete import

--- dragonlinguistics/forms/__init__.py
+++ dragonlinguistics/forms/__init__.py
@@ -1,3 +1,3 @@
-from .articles import Folder, Article, SpecialArticle
+from .articles import Folder, Article
 from .langs import Language, Search as LanguageSearch
 from .words import Word, Senses, Search as WordSearch
