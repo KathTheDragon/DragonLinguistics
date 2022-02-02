13cdb3764393910038cd7cf17eb24c98016ea846
KathTheDragon <kaththedragon@gmail.com>
2022-02-02 12:39:29 +0000

    Refactor a bit

--- dragonlinguistics/models/references.py
+++ dragonlinguistics/models/references.py
@@ -1,5 +1,7 @@
 from django.core.exceptions import ValidationError
 from django.db import models
+from django.utils.safestring import mark_safe
+from django.utils.text import slugify
 
 class Reference(models.Model):
     author = models.CharField(max_length=255)
@@ -11,15 +13,31 @@ class Reference(models.Model):
     class Meta:
         ordering = ['author', 'year', 'id']
 
-    def urls(self, action):
-        from django.urls import reverse
+    @property
+    def index(self):
         titles = [
             title for (title,) in
             Reference.objects.filter(author=self.author, year=self.year).values_list('title')
         ]
+        return titles.index(self.title)
+
+    def long_str(self):
+        html = f'{self.year} - <a href="{self.link}" target="_blank"><em>{self.title}</em></a>'
+        if self.comment:
+            html += f' ({comment})'
+        return mark_safe(html)
+
+    def short_str(self):
+        author = author.split(',', maxsplit=1)[0]
+        index = chr(self.index + ord('a'))
+        return f'{author} ({self.year}{index})'
+
+    def urls(self, action):
+        from django.urls import reverse
+
         return reverse(
             f'references:{action}',
-            kwargs={'author': self.author, 'year': self.year, 'index': titles.index(self.title)}
+            kwargs={'author': self.author, 'year': self.year, 'index': self.index}
         )
 
     def get_edit_url(self):
