7890992d81764335d252fca440f60109b9285532
KathTheDragon <kaththedragon@gmail.com>
2022-04-18 03:12:07 +0100

    Fix attribute name

--- dragonlinguistics/templatetags/markup/__init__.py
+++ dragonlinguistics/templatetags/markup/__init__.py
@@ -26,5 +26,5 @@ def conditional_escape(text, quote=True):
 class Markup(_markup.Markup):
     def __init__(self):
         super().__init__()
-        self.node_handlers['$'] |= nodes.nodes
-        self.node_handlers['@'] = nodes.objects
+        self.nodes['$'] |= nodes.nodes
+        self.nodes['@'] = nodes.objects
