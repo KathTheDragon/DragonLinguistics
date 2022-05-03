42c94cd9e3347f24793a521923e98df26f7c2e7a
KathTheDragon <kaththedragon@gmail.com>
2022-05-03 14:43:50 +0100

    Remove unused files

--- dragonlinguistics/apps.py
+++ dragonlinguistics/apps.py
@@ -4,6 +4,3 @@ from django.apps import AppConfig
 class DragonLinguisticsConfig(AppConfig):
     name = 'dragonlinguistics'
     verbose_name = 'Dragon Linguistics'
-
-    def ready(self):
-        from . import signals
