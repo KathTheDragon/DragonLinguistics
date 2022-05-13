83c910cdafe082aa5038269fab4de9c69d2bf53e
KathTheDragon <kaththedragon@gmail.com>
2022-05-13 15:26:10 +0100

    Revise class list formatting

--- dragonlinguistics/models/words.py
+++ dragonlinguistics/models/words.py
@@ -1,7 +1,65 @@
+import re
+
 from django.db import models
 
 from . import base
 from .langs import Language
+
+def parse_class_list(lines, index=0, indent=''):
+    options = []
+    groups = []
+    while index < len(lines):
+        line = lines[index]
+        _indent = re.match(r'\s*', line)[0]
+        if _indent == line:
+            pass
+        elif _indent == indent:
+            line = line.strip()
+            if line.endswith(':'):
+                line = line.removesuffix(':')
+                groups.append(parse_class(line))
+                _indent = re.match(r'\s*', lines[index+1])[0]
+                if not _indent.startswith(indent):
+                    raise ValueError(f'indent must increase after a group line (line {index+1})')
+                _options, index = parse_class_list(lines, index+1, _indent)
+                if not indent:
+                    options.extend([(name, merge_group(_options, code)) for code, name in groups])
+                else:
+                    options.extend([option for group in groups for option in merge_group(_options, *group)])
+                groups = []
+                continue
+            elif line.endswith(';'):
+                line = line.removesuffix(';')
+                groups.append(parse_class(line))
+            else:
+                if groups:
+                    raise ValueError(f'incomplete group definition (line {index})')
+                options.append(parse_class(line))
+        elif indent.startswith(_indent):  # Dedent
+            break
+        elif _indent.startswith(indent):  # Indent
+            raise ValueError(f'unexpected indentation (line {index})')
+        else:
+            raise ValueError(f'invalid indentation (line {index})')
+        index += 1
+
+    if groups:
+        raise ValueError(f'incomplete group definition (line {index})')
+    return options, index
+
+
+def parse_class(line):
+    parts = line.split(',')
+    if len(parts) == 1:
+        code = name = parts[0]
+    else:
+        code, name = parts[:2]
+    return code.strip(), name.strip()
+
+
+def merge_group(options, code, name=''):
+    return [(f'{_code} {code}', f'{_name} {name}' if name else _name) for _code, _name in options]
+
 
 class Dictionary(base.Model):
     language = models.OneToOneField(Language, on_delete=models.CASCADE)
@@ -11,20 +69,19 @@ class Dictionary(base.Model):
     def __str__(self):
         return f'{self.language} Dictionary'
 
-    def get_class_list(self):
-        classes = []
-        for line in self.classes.splitlines():
-            if not line:
-                continue
-            parts = line.split(',')
-            if len(parts) == 1:
-                code, name, group = parts[0], parts[0], ''
-            elif len(parts) == 2:
-                code, name, group = parts[0], parts[1], ''
+    def get_class_options(self):
+        return parse_class_list(self.classes.splitlines())[0]
+
+    def get_class_dict(self):
+        class_dict = {}
+        for option in self.get_class_options():
+            if isinstance(option[1], str):
+                class_dict[option[0]] = option[1]
             else:
-                code, name, group = parts[:3]
-            classes.append((code.strip(), name.strip(), group.strip()))
-        return classes
+                group, options = option
+                for key, value in options:
+                    class_dict[key] = f'{group} ({value})'
+        return class_dict
 
     def url(self):
         from django.urls import reverse
@@ -127,13 +184,7 @@ class Variant(models.Model):
         return self.get_definitions()[0]
 
     def get_lexclass(self):
-        for code, name, group in self.word.dictionary.get_class_list():
-            if self.lexclass == code:
-                if group:
-                    return f'{group} ({name})'
-                else:
-                    return name
-        return 'Unknown'
+        return self.word.dictionary.get_class_dict().get(self.lexclass, 'Unknown')
 
     def get_definitions(self):
         return self.definition.splitlines() or ['']
